19c4d96f23dfdddfd34d84ed257ec9879c59369c
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2025-08-08 21:56:38 +0100

    [Cpu0][Chapter 02] Register Cpu0 targets with the LLVM backend
    Add the Cpu0 big and little-endian targets.
    llvm builds and llc --version reports the Cpu0 targets.

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -38,6 +38,8 @@ StringRef Triple::getArchTypeName(ArchType Kind) {
   case bpfeb:          return "bpfeb";
   case bpfel:          return "bpfel";
   case csky:           return "csky";
+  case cpu0:           return "cpu0";
+  case cpu0el:         return "cpu0el";
   case dxil:           return "dxil";
   case hexagon:        return "hexagon";
   case hsail64:        return "hsail64";
@@ -446,6 +448,8 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
       .Case("armeb", armeb)
       .Case("avr", avr)
       .StartsWith("bpf", BPFArch)
+      .Case("cpu0", cpu0)
+      .Case("cpu0el", cpu0el)
       .Case("m68k", m68k)
       .Case("mips", mips)
       .Case("mipsel", mipsel)
@@ -595,6 +599,8 @@ static Triple::ArchType parseArch(StringRef ArchName) {
                 .Case("thumb", Triple::thumb)
                 .Case("thumbeb", Triple::thumbeb)
                 .Case("avr", Triple::avr)
+                .Case("cpu0", Triple::cpu0)
+                .Case("cpu0el", Triple::cpu0el)
                 .Case("m68k", Triple::m68k)
                 .Case("msp430", Triple::msp430)
                 .Cases("mips", "mipseb", "mipsallegrex", "mipsisa32r6",
@@ -959,6 +965,8 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::avr:
   case Triple::bpfeb:
   case Triple::bpfel:
+  case Triple::cpu0:
+  case Triple::cpu0el:
   case Triple::csky:
   case Triple::hexagon:
   case Triple::hsail64:
@@ -1689,6 +1697,8 @@ unsigned Triple::getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
   case llvm::Triple::arc:
   case llvm::Triple::arm:
   case llvm::Triple::armeb:
+  case llvm::Triple::cpu0:
+  case llvm::Triple::cpu0el:
   case llvm::Triple::csky:
   case llvm::Triple::dxil:
   case llvm::Triple::hexagon:
@@ -1799,6 +1809,8 @@ Triple Triple::get32BitArchVariant() const {
   case Triple::arc:
   case Triple::arm:
   case Triple::armeb:
+  case Triple::cpu0:
+  case Triple::cpu0el:
   case Triple::csky:
   case Triple::dxil:
   case Triple::hexagon:
@@ -1869,6 +1881,8 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::UnknownArch:
   case Triple::arc:
   case Triple::avr:
+  case Triple::cpu0:
+  case Triple::cpu0el:
   case Triple::csky:
   case Triple::dxil:
   case Triple::hexagon:
@@ -2069,6 +2083,7 @@ bool Triple::isLittleEndian() const {
   case Triple::arm:
   case Triple::avr:
   case Triple::bpfel:
+  case Triple::cpu0el:
   case Triple::csky:
   case Triple::dxil:
   case Triple::hexagon:
