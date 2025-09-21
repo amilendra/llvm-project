424ccefda7a28c8544dbcc92740994db7b6d4761
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2025-09-21 15:37:46 +0100

    [H2BLB] Hook up the Subtarget and TargetLowering
    This implements a skeleton of the Subtarget and TargetLowering
    classes for the H2BLB target.
    
    These two classes are required to be able to run the optimizers from
    the middle-end.
    
    After this commit, it is possible to run `clang` with optimizations
    enable for the h2blb target. That being said, the optimizers don't
    do anything different since we didn't implement anything special
    in the cost model and so on. We just plumbed the information through.

---
+++ llvm/lib/Target/H2BLB/H2BLBRegisterInfo.h
@@ -0,0 +1,42 @@
+//===-- H2BLBRegisterInfo.h - H2BLB Register Information Impl ----*- C++-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the H2BLB implementation of the TargetRegisterInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_H2BLB_H2BLBREGISTERINFO_H
+#define LLVM_LIB_TARGET_H2BLB_H2BLBREGISTERINFO_H
+
+#define GET_REGINFO_HEADER
+#include "H2BLBGenRegisterInfo.inc"
+
+namespace llvm {
+
+struct H2BLBRegisterInfo : public H2BLBGenRegisterInfo {
+
+  H2BLBRegisterInfo();
+
+  const MCPhysReg *getCalleeSavedRegs(const MachineFunction *MF) const override;
+  const uint32_t *getCallPreservedMask(const MachineFunction &MF,
+                                       CallingConv::ID CC) const override;
+  const uint32_t *getNoPreservedMask() const override;
+
+  BitVector getReservedRegs(const MachineFunction &MF) const override;
+  const TargetRegisterClass *getPointerRegClass(
+
+      unsigned Kind) const override;
+  bool eliminateFrameIndex(MachineBasicBlock::iterator MI, int SPAdj,
+                           unsigned FIOperandNum,
+                           RegScavenger *RS = nullptr) const override;
+
+  Register getFrameRegister(const MachineFunction &MF) const override;
+};
+} // namespace llvm
+
+#endif
