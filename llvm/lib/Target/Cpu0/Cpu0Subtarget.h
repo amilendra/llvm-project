6ab798f1be4d7a566a87525460f9f8671ce94681
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2025-10-01 22:47:19 +0100

    [Cpu0][Chapter8_2] 8.3. Long branch support

--- llvm/lib/Target/Cpu0/Cpu0Subtarget.h
+++ llvm/lib/Target/Cpu0/Cpu0Subtarget.h
@@ -65,7 +65,7 @@ public:
 
   bool hasChapter8_1() const { return true; }
 
-  bool hasChapter8_2() const { return false; }
+  bool hasChapter8_2() const { return true; }
 
   bool hasChapter9_1() const { return false; }
 
