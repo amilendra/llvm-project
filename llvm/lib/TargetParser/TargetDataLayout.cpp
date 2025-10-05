7f0a6cd8e5423e81273c56ad970b38334312d280
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2025-10-05 09:56:47 +0100

    [clang] Add a new target h2blb
    The target doesn't do anything fancy, but at least it is hooked up
    and can produce IR.
    
    In other words, after this changes, `clang -target=h2blb` should
    do something sensible with respect to the target triple and the
    data layout.
    
    Regarding the specs of the h2blb backend, we described the following
    elements:
    - little-endian (lsb is at the lowest address)
    - Pointer sizes are 16-bit (i.e., only 64KB of addressable space)
    - Native support of 16 and 32-bit integers
    - Alignments are:
    -- 32-bit for 32-bit type
    -- 16-bit for 16-bit type
    -- 8-bit for 1-bit type
    -- 32-bit f32 (single precision floating-point type)
    -- 32-bit for 32-bit vector types (e.g., <4 x i8>, <2 x i16>)
    
    This specification is completely made up and has been chosen to demonstrate
    how to implement different features in the backend.

--- llvm/lib/TargetParser/TargetDataLayout.cpp
+++ llvm/lib/TargetParser/TargetDataLayout.cpp
@@ -115,6 +115,11 @@ static std::string computeCSKYDataLayout(const Triple &TT) {
          "-v128:32:32-a:0:32-Fi32-n32";
 }
 
+static std::string computeH2BLBDataLayout(const Triple &TT) {
+  std::string Ret = "";
+  return Ret;
+}
+
 static std::string computeLoongArchDataLayout(const Triple &TT) {
   if (TT.isLoongArch64())
     return "e-m:e-p:64:64-i64:64-i128:128-n32:64-S128";
@@ -553,6 +558,8 @@ std::string Triple::computeDataLayout(StringRef ABIName) const {
     // See https://github.com/llvm/llvm-project/issues/123968
     return "e-m:e-p:32:32-i1:32-i8:8-i16:16-i32:32-i64:64-f16:16-"
            "f32:32-f64:64-n8:16:32:64-v48:16:16-v96:32:32-v192:64:64";
+  case Triple::h2blb:
+    return computeH2BLBDataLayout(*this);
   case Triple::hexagon:
     return "e-m:e-p:32:32:32-a:0-n16:32-"
            "i64:64:64-i32:32:32-i16:16:16-i1:8:8-f32:32:32-f64:64:64-"
--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -39,6 +39,7 @@ StringRef Triple::getArchTypeName(ArchType Kind) {
   case bpfel:          return "bpfel";
   case csky:           return "csky";
   case dxil:           return "dxil";
+  case h2blb:          return "h2blb";
   case hexagon:        return "hexagon";
   case hsail64:        return "hsail64";
   case hsail:          return "hsail";
@@ -199,6 +200,8 @@ StringRef Triple::getArchTypePrefix(ArchType Kind) {
   case mipsel:
   case mips64:
   case mips64el:    return "mips";
+
+  case h2blb:       return "h2blb";
 
   case hexagon:     return "hexagon";
 
@@ -454,6 +457,7 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
       .Case("armeb", armeb)
       .Case("avr", avr)
       .StartsWith("bpf", BPFArch)
+      .Case("h2blb", h2blb)
       .Case("m68k", m68k)
       .Case("mips", mips)
       .Case("mipsel", mipsel)
@@ -603,6 +607,7 @@ static Triple::ArchType parseArch(StringRef ArchName) {
           .Case("armeb", Triple::armeb)
           .Case("thumb", Triple::thumb)
           .Case("thumbeb", Triple::thumbeb)
+          .Case("h2blb", Triple::h2blb)
           .Case("avr", Triple::avr)
           .Case("m68k", Triple::m68k)
           .Case("msp430", Triple::msp430)
@@ -956,6 +961,7 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::aarch64:
   case Triple::aarch64_32:
   case Triple::arm:
+  case Triple::h2blb:
   case Triple::thumb:
   case Triple::x86:
   case Triple::x86_64:
@@ -1699,6 +1705,7 @@ unsigned Triple::getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
     return 0;
 
   case llvm::Triple::avr:
+  case llvm::Triple::h2blb:
   case llvm::Triple::msp430:
     return 16;
 
@@ -1819,6 +1826,7 @@ Triple Triple::get32BitArchVariant() const {
   case Triple::armeb:
   case Triple::csky:
   case Triple::dxil:
+  case Triple::h2blb:
   case Triple::hexagon:
   case Triple::hsail:
   case Triple::kalimba:
@@ -1889,6 +1897,7 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::avr:
   case Triple::csky:
   case Triple::dxil:
+  case Triple::h2blb:
   case Triple::hexagon:
   case Triple::kalimba:
   case Triple::lanai:
@@ -1976,6 +1985,7 @@ Triple Triple::getBigEndianArchVariant() const {
   case Triple::amdil:
   case Triple::avr:
   case Triple::dxil:
+  case Triple::h2blb:
   case Triple::hexagon:
   case Triple::hsail64:
   case Triple::hsail:
@@ -2089,6 +2099,7 @@ bool Triple::isLittleEndian() const {
   case Triple::bpfel:
   case Triple::csky:
   case Triple::dxil:
+  case Triple::h2blb:
   case Triple::hexagon:
   case Triple::hsail64:
   case Triple::hsail:
