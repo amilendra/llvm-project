c8aad2100bca0abd6f145ebdf90a1714a3b9a62f
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2025-10-31 07:50:49 +0000

    [Cpu0][Chapter12_1] 12. C++ support

--- llvm/lib/Target/Cpu0/Cpu0RegisterInfo.cpp
+++ llvm/lib/Target/Cpu0/Cpu0RegisterInfo.cpp
@@ -32,6 +32,11 @@ using namespace llvm;
 Cpu0RegisterInfo::Cpu0RegisterInfo(const Cpu0Subtarget &ST)
     : Cpu0GenRegisterInfo(Cpu0::LR), Subtarget(ST) {}
 
+const TargetRegisterClass *
+Cpu0RegisterInfo::getPointerRegClass(unsigned Kind) const {
+  return &Cpu0::CPURegsRegClass;
+}
+
 //===----------------------------------------------------------------------===//
 // Callee Saved Registers methods
 //===----------------------------------------------------------------------===//
