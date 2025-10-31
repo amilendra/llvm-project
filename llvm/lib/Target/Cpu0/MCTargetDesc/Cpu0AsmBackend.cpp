a8d3a997082773d30bad4f56fc4470eaee9cc851
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2025-10-31 00:40:29 +0000

    [Cpu0][Chapter5_1] 5. Support for obj file generation

---
+++ llvm/lib/Target/Cpu0/MCTargetDesc/Cpu0AsmBackend.cpp
@@ -0,0 +1,146 @@
+//===-- Cpu0AsmBackend.cpp - Cpu0 Asm Backend  ------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the Cpu0AsmBackend class.
+//
+//===----------------------------------------------------------------------===//
+//
+
+#include "MCTargetDesc/Cpu0AsmBackend.h"
+#include "MCTargetDesc/Cpu0FixupKinds.h"
+
+#include "MCTargetDesc/Cpu0MCTargetDesc.h"
+#include "llvm/MC/MCAsmBackend.h"
+#include "llvm/MC/MCAssembler.h"
+#include "llvm/MC/MCDirectives.h"
+#include "llvm/MC/MCELFObjectWriter.h"
+#include "llvm/MC/MCObjectWriter.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+static cl::opt<bool> HasLLD("has-lld", cl::init(false),
+                            cl::desc("CPU0: Has lld linker for Cpu0."),
+                            cl::Hidden);
+
+//@adjustFixupValue {
+// Prepare value for the target space for it
+static unsigned adjustFixupValue(const MCFixup &Fixup, uint64_t Value,
+                                 MCContext &Ctx) {
+
+  unsigned Kind = Fixup.getKind();
+
+  // Add/subtract and shift
+  switch (Kind) {
+  default:
+    return 0;
+  case Cpu0::fixup_Cpu0_GPREL32:
+  case FK_Data_4:
+  case Cpu0::fixup_Cpu0_LO16:
+    break;
+  case Cpu0::fixup_Cpu0_HI16:
+  case Cpu0::fixup_Cpu0_GOT:
+    // Get the higher 16-bits. Also add 1 if bit 15 is 1.
+    Value = (Value >> 16) & 0xffff;
+    break;
+  }
+
+  return Value;
+}
+//@adjustFixupValue }
+
+std::unique_ptr<MCObjectTargetWriter>
+Cpu0AsmBackend::createObjectTargetWriter() const {
+  return createCpu0ELFObjectWriter(TheTriple);
+}
+
+/// ApplyFixup - Apply the \p Value for given \p Fixup into the provided
+/// data fragment, at the offset specified by the fixup and following the
+/// fixup kind as appropriate.
+void Cpu0AsmBackend::applyFixup(const MCFragment &F, const MCFixup &Fixup,
+                                const MCValue &Target, uint8_t *Data,
+                                uint64_t Value, bool IsResolved) {
+  MCFixupKind Kind = Fixup.getKind();
+  MCContext &Ctx = getContext();
+  Value = adjustFixupValue(Fixup, Value, Ctx);
+
+  if (!Value)
+    return; // Doesn't change encoding.
+
+  // Where do we start in the object
+  unsigned Offset = Fixup.getOffset();
+  // Number of bytes we need to fixup
+  unsigned NumBytes = (getFixupKindInfo(Kind).TargetSize + 7) / 8;
+  // Used to point to big endian bytes
+  unsigned FullSize;
+
+  switch ((unsigned)Kind) {
+  default:
+    FullSize = 4;
+    break;
+  }
+
+  // Grab current value, if any, from bits.
+  uint64_t CurVal = 0;
+
+  for (unsigned i = 0; i != NumBytes; ++i) {
+    unsigned Idx = TheTriple.isLittleEndian() ? i : (FullSize - 1 - i);
+    CurVal |= (uint64_t)((uint8_t)Data[Offset + Idx]) << (i * 8);
+  }
+
+  uint64_t Mask = ((uint64_t)(-1) >> (64 - getFixupKindInfo(Kind).TargetSize));
+  CurVal |= Value & Mask;
+
+  // Write out the fixed up bytes back to the code/data bits.
+  for (unsigned i = 0; i != NumBytes; ++i) {
+    unsigned Idx = TheTriple.isLittleEndian() ? i : (FullSize - 1 - i);
+    Data[Offset + Idx] = (uint8_t)((CurVal >> (i * 8)) & 0xff);
+  }
+}
+
+//@getFixupKindInfo {
+MCFixupKindInfo Cpu0AsmBackend::getFixupKindInfo(MCFixupKind Kind) const {
+  const static MCFixupKindInfo Infos[Cpu0::NumTargetFixupKinds] = {
+      // This table *must* be in same the order of fixup_* kinds in
+      // Cpu0FixupKinds.h.
+      //
+      // name                        offset  bits  flags
+      {"fixup_Cpu0_32", 0, 32, 0},      {"fixup_Cpu0_HI16", 0, 16, 0},
+      {"fixup_Cpu0_LO16", 0, 16, 0},    {"fixup_Cpu0_GPREL16", 0, 16, 0},
+      {"fixup_Cpu0_GOT", 0, 16, 0},     {"fixup_Cpu0_GOT_HI16", 0, 16, 0},
+      {"fixup_Cpu0_GOT_LO16", 0, 16, 0}};
+
+  if (Kind < FirstTargetFixupKind)
+    return MCAsmBackend::getFixupKindInfo(Kind);
+
+  assert(unsigned(Kind - FirstTargetFixupKind) < Cpu0::NumTargetFixupKinds &&
+         "Invalid kind!");
+  return Infos[Kind - FirstTargetFixupKind];
+}
+//@getFixupKindInfo }
+
+/// WriteNopData - Write an (optimal) nop sequence of Count bytes
+/// to the given output. If the target cannot generate such a sequence,
+/// it should return an error.
+///
+/// \return - True on success.
+bool Cpu0AsmBackend::writeNopData(raw_ostream &OS, uint64_t Count,
+                                  const MCSubtargetInfo *STI) const {
+  return true;
+}
+
+// MCAsmBackend
+MCAsmBackend *llvm::createCpu0AsmBackend(const Target &T,
+                                         const MCSubtargetInfo &STI,
+                                         const MCRegisterInfo &MRI,
+                                         const MCTargetOptions &Options) {
+  return new Cpu0AsmBackend(T, STI.getTargetTriple());
+}
