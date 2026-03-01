5ce2d36a85349ee452a6aef69ea3402966ab39c7
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2026-03-01 05:56:18 +0000

    [H2BLB] Hook up the Subtarget and TargetLowering
    This implements a skeleton of the Subtarget and TargetLowering
    classes for the H2BLB target.
    
    These two classes are required to be able to run the optimizers from
    the middle-end.
    
    After this commit, it is possible to run `clang` with optimizations
    enable for the h2blb target. That being said, the optimizers don't
    do anything different since we didn't implement anything special
    in the cost model and so on. We just plumbed the information through.

---
+++ llvm/lib/Target/H2BLB/H2BLBFrameLowering.h
@@ -0,0 +1,19 @@
+//==- H2BLBFrameLowering.h - Define frame lowering for H2BLB -----*- C++ -*-==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_H2BLB_H2BLBFRAMELOWERING_H
+#define LLVM_LIB_TARGET_H2BLB_H2BLBFRAMELOWERING_H
+
+#include "llvm/CodeGen/TargetFrameLowering.h"
+
+namespace llvm {
+
+class H2BLBFrameLowering : public TargetFrameLowering {};
+
+} // namespace llvm
+#endif // LLVM_LIB_TARGET_HEXAGON_HEXAGONFRAMELOWERING_H
