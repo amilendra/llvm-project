f982ad3beaa46144465fc1765fddf859d9b121f0
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2026-08-22 08:49:57 +0100

    [Cpu0][Chapter3_1] 3.1. TargetMachine structure
    Adds the Cpu0 TargetMachine and the cpu032I, cpu032II sub targets.

---
+++ llvm/lib/Target/Cpu0/Cpu0SEInstrInfo.h
@@ -0,0 +1,35 @@
+//===-- Cpu0SEInstrInfo.h - Cpu032/64 Instruction Information ---*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the Cpu032/64 implementation of the TargetInstrInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_CPU0_CPU0SEINSTRINFO_H
+#define LLVM_LIB_TARGET_CPU0_CPU0SEINSTRINFO_H
+
+#include "Cpu0Config.h"
+
+#include "Cpu0InstrInfo.h"
+#include "Cpu0MachineFunction.h"
+#include "Cpu0SERegisterInfo.h"
+
+namespace llvm {
+
+class Cpu0SEInstrInfo : public Cpu0InstrInfo {
+  const Cpu0SERegisterInfo RI;
+
+public:
+  explicit Cpu0SEInstrInfo(const Cpu0Subtarget &STI);
+
+  const Cpu0RegisterInfo &getRegisterInfo() const override;
+};
+
+} // namespace llvm
+
+#endif
