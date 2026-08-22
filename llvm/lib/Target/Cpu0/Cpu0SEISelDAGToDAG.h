ea24149cc6f4683d0a13e70707903ae5aa49acf9
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2026-08-22 08:52:42 +0100

    [Cpu0][Chapter3_3] 3.3. Add Cpu0DAGToDAGISel class

---
+++ llvm/lib/Target/Cpu0/Cpu0SEISelDAGToDAG.h
@@ -0,0 +1,50 @@
+//===-- Cpu0SEISelDAGToDAG.h - A Dag to Dag Inst Selector for Cpu0SE - C++ -*-//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Subclass of Cpu0DAGToDAGISel specialized for cpu032.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_CPU0_CPU0SEISELDAGTODAG_H
+#define LLVM_LIB_TARGET_CPU0_CPU0SEISELDAGTODAG_H
+
+#include "Cpu0Config.h"
+
+#include "Cpu0ISelDAGToDAG.h"
+
+namespace llvm {
+
+class Cpu0SEDAGToDAGISel : public Cpu0DAGToDAGISel {
+
+public:
+  explicit Cpu0SEDAGToDAGISel(Cpu0TargetMachine &TM, CodeGenOptLevel OL)
+      : Cpu0DAGToDAGISel(TM, OL) {}
+
+private:
+  bool runOnMachineFunction(MachineFunction &MF) override;
+
+  bool trySelect(SDNode *Node) override;
+
+  void processFunctionAfterISel(MachineFunction &MF) override;
+
+  // Insert instructions to initialize the global base register in the
+  // first MBB of the function.
+  //  void initGlobalBaseReg(MachineFunction &MF);
+};
+
+class Cpu0SEDAGToDAGISelLegacy : public Cpu0DAGToDAGISelLegacy {
+public:
+  explicit Cpu0SEDAGToDAGISelLegacy(Cpu0TargetMachine &TM, CodeGenOptLevel OL);
+};
+
+FunctionPass *createCpu0SEISelDag(Cpu0TargetMachine &TM,
+                                  CodeGenOptLevel OptLevel);
+
+} // namespace llvm
+
+#endif
