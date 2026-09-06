9ea46421c2f45fb4c025bd7f43401aa294cce946
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2026-09-06 12:59:35 +0100

    [Cpu0][Chapter9_2] 9.3. Function call: Store arguments to stack frame

--- llvm/lib/Target/Cpu0/Cpu0FrameLowering.cpp
+++ llvm/lib/Target/Cpu0/Cpu0FrameLowering.cpp
@@ -97,3 +97,11 @@ bool Cpu0FrameLowering::hasFPImpl(const MachineFunction &MF) const {
   return MF.disableFramePointerElim() || MFI.hasVarSizedObjects() ||
          MFI.isFrameAddressTaken() || TRI->hasStackRealignment(MF);
 }
+
+// Eliminate ADJCALLSTACKDOWN, ADJCALLSTACKUP pseudo instructions
+MachineBasicBlock::iterator Cpu0FrameLowering::eliminateCallFramePseudoInstr(
+    MachineFunction &MF, MachineBasicBlock &MBB,
+    MachineBasicBlock::iterator I) const {
+
+  return MBB.erase(I);
+}
